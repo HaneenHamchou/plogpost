3689a66d9d448deacc22256a6b2987175dc3c437
HaneenHamchou <haneen.hamchou@gmail.com>
2024-11-19 03:51:08 +0100

    Build a Blog Website, and add Pagination to the post list view

---
+++ blogapp/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DR' , 'Draft'
+        PUBLISHED = 'PB' , 'Published'
+
+    title = models.CharField(max_length = 255)
+    slug = models.SlugField(max_length = 255, unique_for_date='publish')
+    author= models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blogapp:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug,
+        ])
+
+
+
---
+++ blogapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# def home(request):
+#     return render(request, 'home.html')
+
+# def about(request):
+#     return render(request, 'about.html')
+
+def post_list(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blogapp\post\list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post, 
+                             publish__year=year, 
+                             publish__month=month, 
+                             publish__day=day, 
+                        )
+    return render(request, 'blogapp/post/detail.html', {'post': post})
