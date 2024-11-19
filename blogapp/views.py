98ccb20962fd280bb03ed64e4995adaac0487dd1
HaneenHamchou <haneen.hamchou@gmail.com>
2024-11-19 04:33:32 +0100

    Class-based-view

--- blogapp/views.py
+++ blogapp/views.py
@@ -2,25 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-# def home(request):
-#     return render(request, 'home.html')
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list, 2)
+#     page_number = request.GET.get('page', 1)
 
-# def about(request):
-#     return render(request, 'about.html')
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blogapp\post\list.html', {'posts': posts})
 
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 2)
-    page_number = request.GET.get('page', 1)
-
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blogapp\post\list.html', {'posts': posts})
+class PostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    paginate_by = 2
+    template_name = 'blogapp\post\list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
