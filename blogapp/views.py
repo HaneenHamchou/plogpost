e8b83d7d550e6fbb6b46bcbf2f5a40fdd7573d94
HaneenHamchou <haneen.hamchou@gmail.com>
2024-12-16 12:35:25 +0100

    Add new file

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from blogapp.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
+
+
 
 # def post_list(request):
 #     post_list = Post.objects.all()
@@ -24,10 +30,46 @@ class PostListView(ListView):
     template_name = 'blogapp\post\list.html'
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+    post = get_object_or_404(Post,
                              slug=post, 
                              publish__year=year, 
                              publish__month=month, 
                              publish__day=day, 
                         )
-    return render(request, 'blogapp/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    return render(request, 'blogapp/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        #Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} \n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'haneen.hamchou@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blogapp/post/share.html', {'post': post, 'form': form, 'sent': sent})
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the DB
+        comment = form.save(commit=False)
+        # Assign the post to the Comment
+        comment.post = post
+        # Save the Comment to the DB
+        comment.save()
+    return render(request, 'blogapp/post/comment.html', {'post':post, 'form':form, 'comment':comment})
--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-34on!iv_)ejh&s+5k=0gsc%c%h4^)#ru(dkyen$u&gx19eph^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogapp.apps.BlogappConfig'
+    'blogapp.apps.BlogappConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com '
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'haneen.hamchou@gmail.com'
+EMAIL_HOST_PASSWORD = 'kdsd xfgr lvoe rbur'
+
+
